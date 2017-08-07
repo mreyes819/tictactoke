a78ab7929f65e98d662503165db1f4f623b8dfe2
matt <mttrys@gmail.com>
2017-08-07 15:36:56 -0700

    most of the logic is here

--- tictactoe.js
+++ tictactoe.js
@@ -10,6 +10,11 @@ var TicTacToe = functoin() {
     [, , ]
   ];
 
+  while(this.winner !== true) {
+    playersMove();
+  }
+
+  return;
 }
 
 var gameMethods = function() {
@@ -23,19 +28,30 @@ var gameMethods = function() {
        this.board[0][0] === this.board[1][1] === this.board[2][2] && (this.board[0][0] !== undefined) ||
        this.board[0][2] === this.board[1][1] === this.board[2][0] && (this.board[0][2] !== undefined)
        ) {
-        return winner = true; // how to find who is winner
+        winner = true; // how to find who is winner
     }
   }
 
-  playersMove: function(x,y) {
+  playersMove: function() {
     if (this.moveCount % 2 === 0) {
       this.currentPlayer = 'X';
     } else {
       this.currentPlayer = 'Y';
     }
 
+    console.log(`Player ${this.currentPlayer}'s move. Enter row: `)
+    //var row = get users input
+    console.log(`Player ${this.currentPlayer}'s move. Enter col: `)
+    // var col = get users input
 
-
+    // if valid move
+      // insert move onto board
+      // this.movecount++;
+      // check to see if winner
+      // if winner = true
+        // console.log this.currentPlayer won!
+    // else
+      // invalid move. try again
   }
 
 }
